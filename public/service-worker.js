8fbe7c88c49780baa86d446344a3831d952041c5
Sharlene May <sharleneminosa@gmail.com>
2020-06-19 09:01:40 -0700

    added install event activate event and fetch event

--- public/service-worker.js
+++ public/service-worker.js
@@ -16,11 +16,47 @@ self.addEventListener('install', (event) => {
         .open(cacheName)
         .then(cache => {
             console.log('Service Worker: Caching Files');
+            cache.addAll(cacheAssets);
         })
-    )
+        .then( () => { self.skipWaiting()})
+    );
 });
 
 // call activate event
 self.addEventListener('activate', (event) => {
-    console.log('Service Worker: Activated');
+    // console.log('Service Worker: Activated');
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map (cache => {
+                    if(cache!==cacheName){
+                        console.log('Service Woerker: Clearing Old Cache');
+                        return caches.delete(chache);
+                    };
+                })
+            );
+        })
+    );
+});
+
+// call fetch event
+self.addEventListener('fetch', event => {
+    console.log('Service Worker: Fetching');
+    event.respondWith(
+        fetch(event.request)
+        // .then(response => {
+        //     // make copy/clone of response
+        //     const responseClone = response.clone();
+        //     // open cache
+        //     caches.open(cacheName)
+        //     .then(cache => {
+        //         // add response to cache
+        //         cache.put(event.request,responseClone);
+        //     });
+        //     return response;
+        // })
+        .catch(() => {
+            caches.match(event.request)
+        })
+    );
 });
